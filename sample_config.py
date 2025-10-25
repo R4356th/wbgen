deaac2b4255b4344de8f6267ee5e26492efd61fb
Radman Siddiki <61620631+R4356th@users.noreply.github.com>
2025-10-25 21:23:40 +0600

    Add more comments explaining some config vars

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,5 @@
-
-REPO_API_URL = ""
-WIKI_API_URL = ""
+REPO_API_URL = "" # This is the API endpoint of the Wikibase repository.
+WIKI_API_URL = "" # This is the API endpoint of the wiki where the articles are to be made.
 USERAGENT = ""
 USERNAME = ""
 PASSWORD = ""
@@ -8,7 +7,7 @@ DEEPSEEK_API_KEY = ""
 OPENROUTER_API_KEY = ""
 CUSTOM_API_KEY = 'sk-local' # This is required by the openai library even if it is not actually required by the inference provider.
 CUSTOM_API_URL = 'http://localhost:8080/v1' # This is for llama.cpp. The port can be changed.
-DBName = "wiki"
+DBName = "wiki" # This is the database name that would be found in the API response telling us whether a page is already linked to the wiki with this database name.
 def custom_sys_prompt() -> str:
     return "Avoid using disambiguation-style links and external links. Remember that the wiki is about <topic> only."
 def summary(item: str) -> str:
